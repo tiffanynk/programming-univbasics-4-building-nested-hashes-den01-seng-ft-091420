a40afe33e0815bc64a280c5949e1b8d88cb54a48
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-09 00:19:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
         one_piece_owned: 25,
         two_pieces_owned: 50,
         three_pieces_owned: 100,
-        four_pieces_owned: 200s
+        four_pieces_owned: 200
       },
       names: {
         reading_railroad: {},
